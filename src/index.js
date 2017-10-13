2af7eb60be649116c6a6270f9a164916069326d3
zhanglong <zoran13@126.com>
2017-10-13 17:55:01 +0800

    ff

--- src/index.js
+++ src/index.js
@@ -10,44 +10,35 @@ const appState = {
   }
 }
 
-// function dispatch (action) {
-//   switch (action.type) {
-//     case 'UPDATE_TITLE_TEXT':
-//       appState.title.text = action.text
-//       break
-//     case 'UPDATE_TITLE_COLOR':
-//       appState.title.color = action.color
-//       break
-//     default:
-//       break
-//   }
-// }
-
-
-function renderApp (appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
+function renderApp (newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return
+  console.log('render app...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
 }
 
-function renderTitle (title) {
+function renderTitle (newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return
+  console.log('render title...')
   const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
 }
 
-function renderContent (content) {
+function renderContent (newContent, oldContent = {}) {
+  if (newContent === oldContent) return
+  console.log('render content...')
   const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
 }
 
 function createStore (state, stateChanger) {
   const listeners = []
-  debugger
   const subscribe = (listener) => listeners.push(listener)
   const getState = () => state
   const dispatch = (action) => {
-    stateChanger(state, action)
+    state = stateChanger(state, action)
     listeners.forEach((listener) => listener())
   }
   return { getState, dispatch, subscribe }
@@ -56,18 +47,33 @@ function createStore (state, stateChanger) {
 function stateChanger (state, action) {
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
-      state.title.text = action.text
-      break
+      return { // 构建新的对象并且返回
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      state.title.color = action.color
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
     default:
-      break
+      return state // 没有修改,返回原来的对象
   }
 }
 
 const store = createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState()))
+let oldState = store.getState()
+store.subscribe(() => {
+  const newState = store.getState() // 数据可能变化，获取新的 state
+  renderApp(newState, oldState) // 把新旧的 state 传进去渲染
+  oldState = newState
+})
 
 renderApp(store.getState())
 store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'})
